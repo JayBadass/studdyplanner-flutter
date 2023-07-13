1747331356241d5f68ebab3d386fb2a07b14fcb9
JayBadass <jaybadass@naver.com>
2023-07-13 14:56:34 +0900

    Memo Class에 타이틀 파라미터 추가

--- lib/memo_service.dart
+++ lib/memo_service.dart
@@ -5,10 +5,12 @@ import 'main.dart';
 // Memo 데이터의 형식을 정해줍니다. 추후 isPinned, updatedAt 등의 정보도 저장할 수 있습니다.
 class Memo {
   Memo({
+    required this.title,
     required this.content,
     this.isChecked = false,
   });
 
+  String title;
   String content;
   bool isChecked;
 }
@@ -16,18 +18,19 @@ class Memo {
 // Memo 데이터는 모두 여기서 관리
 class MemoService extends ChangeNotifier {
   List<Memo> memoList = [
-    Memo(content: '장보기 목록: 사과, 양파'), // 더미(dummy) 데이터
-    Memo(content: '새 메모'), // 더미(dummy) 데이터
+    Memo(title: '18조', content: '화이팅'), // 더미(dummy) 데이터
   ];
 
-  createMemo({required String content}) {
-    Memo memo = Memo(content: content);
+  createMemo({required String title, required String content}) {
+    Memo memo = Memo(title: title, content: content);
     memoList.add(memo);
-    notifyListeners(); // Consumer<MemoService>의 builder 부분을 호출해서 화면 새로고침
+    notifyListeners();
   }
 
-  updateMemo({required int index, required String content}) {
+  updateMemo(
+      {required int index, required String title, required String content}) {
     Memo memo = memoList[index];
+    memo.title = title;
     memo.content = content;
     notifyListeners();
   }
