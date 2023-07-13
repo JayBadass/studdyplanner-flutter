09e0d619a801e8c21fd11f80e3ccff73d5f9b15c
JayBadass <jaybadass@naver.com>
2023-07-13 12:06:04 +0900

    detail page 구현

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'memo_service.dart';
+
+// 메모 생성 및 수정 페이지
 class DetailPage extends StatelessWidget {
-  DetailPage({super.key, required this.memoList, required this.index});
+  DetailPage({super.key, required this.index});
 
-  final List<String> memoList;
   final int index;
 
   TextEditingController contentController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    contentController.text = memoList[index];
+    MemoService memoService = context.read<MemoService>();
+    Memo memo = memoService.memoList[index];
+
+    contentController.text = memo.content;
 
     return Scaffold(
       appBar: AppBar(
@@ -19,35 +25,7 @@ class DetailPage extends StatelessWidget {
           IconButton(
             onPressed: () {
               // 삭제 버튼 클릭시
-              showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    title: Text("정말로 삭제하시겠습니까?"),
-                    actions: [
-                      // 취소 버튼
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child: Text("취소"),
-                      ),
-                      // 확인 버튼
-                      TextButton(
-                        onPressed: () {
-                          memoList.removeAt(index); // index에 해당하는 항목 삭제
-                          Navigator.pop(context); // 팝업 닫기
-                          Navigator.pop(context); // HomePage 로 가기
-                        },
-                        child: Text(
-                          "확인",
-                          style: TextStyle(color: Colors.pink),
-                        ),
-                      ),
-                    ],
-                  );
-                },
-              );
+              showDeleteDialog(context, memoService);
             },
             icon: Icon(Icons.delete),
           )
@@ -58,7 +36,7 @@ class DetailPage extends StatelessWidget {
         child: TextField(
           controller: contentController,
           decoration: InputDecoration(
-            hintText: "해야 할 일을 입력하세요",
+            hintText: "메모를 입력하세요",
             border: InputBorder.none,
           ),
           autofocus: true,
@@ -67,10 +45,42 @@ class DetailPage extends StatelessWidget {
           keyboardType: TextInputType.multiline,
           onChanged: (value) {
             // 텍스트필드 안의 값이 변할 때
-            memoList[index] = value;
+            memoService.updateMemo(index: index, content: value);
           },
         ),
       ),
+    );
+  }
+
+  void showDeleteDialog(BuildContext context, MemoService memoService) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("정말로 삭제하시겠습니까?"),
+          actions: [
+            // 취소 버튼
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("취소"),
+            ),
+            // 확인 버튼
+            TextButton(
+              onPressed: () {
+                memoService.deleteMemo(index: index);
+                Navigator.pop(context); // 팝업 닫기
+                Navigator.pop(context); // HomePage 로 가기
+              },
+              child: Text(
+                "확인",
+                style: TextStyle(color: Colors.pink),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
